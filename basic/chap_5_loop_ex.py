61b336706222d07f345e99bc074149dddb147405
victoryu <victorwv98@gmail.com>
2024-08-20 22:15:33 +0800

    add a loop exercise chap_5_loop_ex

---
+++ basic/chap_5_loop_ex.py
@@ -0,0 +1,19 @@
+sum=0
+for x in range(101):
+    sum=sum+x
+print(sum)
+
+
+sum =0
+n=0
+while(n<=100):
+    sum=sum+n
+    n=n+1
+
+print(sum)
+
+L=['Bart', 'Lisa', 'Adam']
+
+for x in L:
+    print('Hello, %s!'%x)
+
