80bafa826cb4167d11970627b8deed153a38d11f
victoryu <victorwv98@gmail.com>
2024-09-02 16:18:47 +0800

    add a returning function exercise

---
+++ chap8_functional_programming/returning_function_ex.py
@@ -0,0 +1,29 @@
+# Use a closure to return a counter function that returns an incrementing integer each time it is called:
+
+# def createCounter():
+#     def counter():
+#         return 1
+#     return counter
+
+# # Test:
+# counterA = createCounter()
+# print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+# counterB = createCounter()
+# if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+#     print('Test passed!')
+# else:
+#     print('Test failed!')
+
+def createCounter():
+    def counter():
+        return 1
+    return counter
+
+# 测试:
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+    print('Test passed!')
+else:
+    print('Test failed!')
