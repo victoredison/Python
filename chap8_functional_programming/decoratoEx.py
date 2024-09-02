35ce33e3a217c97d393acb831d19ecfaab6e6423
victoryu <victorwv98@gmail.com>
2024-09-02 17:30:15 +0800

    add a decorator exercise

--- chap8_functional_programming/decoratoEx.py
+++ chap8_functional_programming/decoratoEx.py
@@ -1,3 +1,32 @@
+
+# Please design a decorator that can be applied to any function and print the execution time of that function:
+# python
+# 
+# import time, functools
+
+# def metric(fn):
+#     print('%s executed in %s ms' % (fn.__name__, 10.24))
+#     return fn
+
+# # Test
+# @metric
+# def fast(x, y):
+#     time.sleep(0.0012)
+#     return x + y
+
+# @metric
+# def slow(x, y, z):
+#     time.sleep(0.1234)
+#     return x * y * z
+
+# f = fast(11, 22)
+# s = slow(11, 22, 33)
+# if f != 33:
+#     print('Test failed!')
+# elif s != 7986:
+#     print('Test failed!')
+
+
 import time, functools
 
 def metric(fn):
