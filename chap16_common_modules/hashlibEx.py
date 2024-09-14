7d403f4e618782a14919de8bf3f0dac92cd2011f
victoryu <victorwv98@gmail.com>
2024-09-14 17:04:51 +0800

    add a hashlib exercise

---
+++ chap16_common_modules/hashlibEx.py
@@ -0,0 +1,41 @@
+import hashlib, random
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' an exercise of hashlib module'
+
+__author__ = 'victor yu'
+
+
+def get_md5(pws):
+       return  hashlib.md5(pws.encode('UTF-8')).hexdigest()
+
+
+class User(object):
+
+    def __init__(self, username, password):
+        self.username = username
+        self.salt = ''.join([chr(random.randint(48, 122)) for i in range(20)])
+        self.password = get_md5(password + self.salt)
+
+db = {
+    'michael': User('michael', '123456'),
+    'bob': User('bob', 'abc999'),
+    'alice': User('alice', 'alice2008')
+}
+
+
+def login(username, password):
+    user = db[username]
+    return user.password == get_md5(password+user.salt)
+
+# 测试:
+assert login('michael', '123456')
+assert login('bob', 'abc999')
+assert login('alice', 'alice2008')
+assert not login('michael', '1234567')
+assert not login('bob', '123456')
+assert not login('alice', 'Alice2008')
+print('ok')
