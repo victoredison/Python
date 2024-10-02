e09e73fcc4c49b04bb0abd470a0b2cd594a7bd73
victoryu <victorwv98@gmail.com>
2024-10-02 16:41:55 -0400

    add a simple  GUI example

---
+++ chap18_gui/guiex.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# an exercise of GUI directly from the tutorial
+
+__author__ = 'victor yu'
+
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='Hello', command=self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message', 'Hello, %s' % name)
+
+app = Application()
+# set up the frame:
+app.master.title('Hello World')
+# main message loop:
+app.mainloop()
